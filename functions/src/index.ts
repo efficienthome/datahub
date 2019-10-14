a975ef3fe8a10e03fcbbda1cf2d5ee7ab6d30e67
msorokin <mikhail.sorokin@sap.com>
2019-10-14 11:38:02 +0300

    initial

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
 import * as functions from 'firebase-functions';
 
-export const test2 = functions.https.onRequest((request, response) => {
+export const test = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
